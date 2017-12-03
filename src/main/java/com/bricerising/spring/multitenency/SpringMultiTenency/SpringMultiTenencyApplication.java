7b68d0f3c2cd22f5e1089dde0a4b7f494365563e
Brice Rising <rising.brice@gmail.com>
2017-12-03 11:09:01 -0500

    Initialize spring-boot project
    Used spring initializr to create a base springboot project for this
    repository.

---
+++ src/main/java/com/bricerising/spring/multitenency/SpringMultiTenency/SpringMultiTenencyApplication.java
@@ -0,0 +1,12 @@
+package com.bricerising.spring.multitenency.SpringMultiTenency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultiTenencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMultiTenencyApplication.class, args);
+	}
+}
